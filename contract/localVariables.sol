273664416536a6b3804fa3ac14f4993c8c0f5420
anil0403 <anilshrestha0403.as@gmail.com>
2023-05-31 21:18:53 +0545

    global variables

---
+++ contract/localVariables.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract LocalVariables {
+    uint256 public myNumber;
+
+    // function for local variables
+    function local()
+        public
+        returns (
+            address,
+            uint256,
+            uint256
+        )
+    {
+        // variabes defined inside the function scope are local variables
+        // not stored on the blockchain but stored on the memory
+
+        uint256 i = 345;
+        myNumber = i;
+        i += 45;
+        address myAddress = address(1);
+        return (myAddress, myNumber, i);
+    }
+}
